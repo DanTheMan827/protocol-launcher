94c8d139577dc8e50bb70a7556e85ee5525fd8fd
Daniel <DanTheMan827>
2016-05-10 09:18:48 -0500

    First Commit

---
+++ modMain.vb
@@ -0,0 +1,12 @@
+﻿Imports System.IO
+Module modMain
+
+    Sub Main(ByVal sArgs() As String)
+        If sArgs.Length = 1 Then
+            Process.Start(sArgs(0))
+            Console.WriteLine("Press any key to continue . . .")
+            Console.ReadKey()
+        End If
+    End Sub
+
+End Module
